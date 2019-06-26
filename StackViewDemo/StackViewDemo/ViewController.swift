2050deea73b8cce8655241174aa4f1d97722932b
Diana Duan <diana.duan@rea-group.com>
2019-06-27 09:02:48 +1000

    t first commit to add stackview

---
+++ StackViewDemo/StackViewDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StackViewDemo
+//
+//  Created by Diana Duan on 27/6/19.
+//  Copyright © 2019 AppCoda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
